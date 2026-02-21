c6597fecccdbf1340aa978c6c6ca07d834d605a1
AbhimanyuRajoriya <abhimanyusaini294@gmail.com>
2026-02-21 11:22:29 +0530

    message

--- frontend/src/screens/Cart.jsx
+++ frontend/src/screens/Cart.jsx
@@ -42,7 +42,7 @@ const Cart = () => {
       }));
 
       // Use the globalFetch function
-      const result = await globalFetch('http://localhost:5000/api/orderData', {
+      const result = await globalFetch('http://3.90.179.122:5000/api/orderData', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
